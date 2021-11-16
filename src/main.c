7072d0d2905400ff4df01315e5994e8b3674977f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-11-16 01:42:10 -0500

    f WIP main still

--- src/main.c
+++ src/main.c
@@ -5,8 +5,10 @@
 #include <lv_drv_conf.h>
 
 #include <hal.h>
+#include <scale.h>
 #include <lv_lib_freetype/lv_freetype.h>
 #include <lv_lib_nanosvg/lv_nanosvg.h>
+#include "theme.h"
 
 #define FRAME_RATE 1 / 60
 #define SECOND_AS_MICROSECONDS 1000000
@@ -14,6 +16,17 @@
 #if USE_MONITOR
 extern int monitor_height;
 extern int monitor_width;
+#endif
+
+#define INSTALL_TO_SPI 0
+#define INSTALL_TO_EMMC 1
+
+#if INSTALL_TO_SPI
+#define INSTALL_LOCATION "SPI Flash"
+#elif INSTALL_TO_EMMC
+#define INSTALL_LOCATION "eMMC"
+#else
+#error INSTALL_TO_XXX not given
 #endif
 
 static void btn_event_cb(lv_obj_t * btn, lv_event_t event);
@@ -28,13 +41,18 @@ enum app_actions {
 };
 typedef enum app_actions app_actions_t;
 
+struct window;
 typedef struct window {
 	lv_obj_t* container;
 	lv_obj_t* scr;
 	lv_obj_t* main_container;
 	lv_obj_t* actions_container;
 	lv_obj_t* top_container;
+
+	struct window* back_location;
 } window_t;
+
+window_t* current_window;
 
 lv_obj_t* add_button(lv_obj_t* parent, app_actions_t action, char* label_text)
 {
@@ -79,29 +97,27 @@ window_t* create_window(char* title_text)
 	lv_cont_set_fit2(container, LV_FIT_FLOOD, LV_FIT_FLOOD);
 	lv_cont_set_style(container, LV_CONT_STYLE_MAIN, &lv_style_transp_fit);
 	
-	lv_obj_t * top_container = add_container(container, false);
+	lv_obj_t * top_container = add_container(container, true);
 	lv_obj_t * main_container = add_container(container, true);
 	lv_cont_set_fit2(main_container, LV_FIT_NONE, LV_FIT_NONE);
 	lv_obj_t * actions_container = add_container(container, true);
 
-	//lv_obj_t * top_container = lv_cont_create(container, NULL);
-	//lv_cont_set_layout(top_container, LV_LAYOUT_COL_M);
-	//lv_cont_set_fit2(top_container, LV_FIT_NONE, LV_FIT_TIGHT);
-	////lv_cont_set_style(top_container, LV_CONT_STYLE_MAIN, &lv_style_transp_fit);
-	//
-	//lv_obj_t * main_container = lv_cont_create(container, NULL);
-	//lv_cont_set_layout(main_container, LV_LAYOUT_COL_M);
-	//lv_cont_set_fit2(main_container, LV_FIT_NONE, LV_FIT_NONE);
-	//lv_cont_set_style(main_container, LV_CONT_STYLE_MAIN, &lv_style_transp_fit);
-	//
-	//lv_obj_t * actions_container = lv_cont_create(container, NULL);
-	//lv_cont_set_layout(actions_container, LV_LAYOUT_COL_M);
-	//lv_cont_set_fit2(actions_container, LV_FIT_NONE, LV_FIT_TIGHT);
-	//lv_cont_set_style(actions_container, LV_CONT_STYLE_MAIN, &lv_style_transp_fit);
+	lv_obj_t * logo = lv_img_create(top_container, NULL);
+	char* logo_path = calloc(PATH_MAX, sizeof(char));
+	sprintf(
+		logo_path,
+		"%s?width=%d",
+		hal_asset_path("installer.svg"),
+		lv_obj_get_width_fit(top_container)
+	);
+	lv_img_set_src(logo, logo_path);
+	free(logo_path);
 
+#if 0
     lv_obj_t * title = lv_label_create(top_container, NULL);
     lv_label_set_text(title, title_text);
 	lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+#endif
 
 	window_t* window = calloc(1, sizeof(window_t));
 	window->container = container;
@@ -121,7 +137,7 @@ void finalize_window(window_t* window)
 	// Resizes the main element.
 	// LVGL at the current release segfaults trying to LV_FIT_FILL in this situation.
 	// XXX: get actual window->container padding
-	int padding = 12;
+	int padding = POINTS_SCALE(10);
 	lv_obj_set_height(
 		window->main_container,
 		(
@@ -133,17 +149,16 @@ void finalize_window(window_t* window)
 	);
 }
 
-void tbgui_main_window_init(void)
+window_t* tbgui_main_window_init(void)
 {
-	window_t* window = create_window("Tow-Boot SPI installer");
+	window_t* window = create_window("Tow-Boot " INSTALL_LOCATION " installer");
 
     lv_obj_t * description_label = lv_label_create(window->main_container, NULL);
 	lv_label_set_long_mode(description_label, LV_LABEL_LONG_BREAK);
     lv_obj_set_width(description_label, lv_obj_get_width_fit(window->main_container));
-	// TODO: read text from configuration
     lv_label_set_text(
 		description_label,
-		"\nThis application is used to manage the Tow-Boot installation on SPI.\n\nUse one of the following options.\n"
+		"\nThis application is used to manage the Tow-Boot installation on " INSTALL_LOCATION ".\n\nUse one of the following options.\n"
 	);
 
 	// Buttons
@@ -151,13 +166,15 @@ void tbgui_main_window_init(void)
 	add_button(
 		window->actions_container,
 		APP_ACTION_INSTALL,
-		"Install Tow-Boot to SPI"
+		"Install Tow-Boot to " INSTALL_LOCATION
 	);
+#if INSTALL_TO_SPI
 	add_button(
 		window->actions_container,
 		APP_ACTION_ERASE,
-		"Erase SPI Flash content"
+		"Erase " INSTALL_LOCATION " content"
 	);
+#endif
 	add_button(
 		window->actions_container,
 		APP_ACTION_POWEROFF,
@@ -168,6 +185,8 @@ void tbgui_main_window_init(void)
 
 	lv_cont_set_layout(window->main_container, LV_LAYOUT_OFF);
 	lv_obj_align(description_label, NULL, LV_ALIGN_CENTER, 0, 0);
+
+	return window;
 }
 
 static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
@@ -179,7 +198,8 @@ static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
 			case APP_ACTION_NONE:
 				break;
 			case APP_ACTION_BACK:
-				printf("Will go back to main screen...\n");
+				printf("Will go back to currentwindow->back_location...\n");
+				// present_window(current_window->back_location);
 				break;
 			case APP_ACTION_INSTALL:
 				printf("Will install...\n");
@@ -207,11 +227,32 @@ static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
 }
 
 
+void font_init()
+{
+	int ret = 0;
+
+	// Landscape mode?
+	if (disp_drv.hor_res > disp_drv.ver_res) {
+		// This app is tight in landscape mode since we show the portrait mode app centered.
+		// Let's make the scaling work out better.
+		mn_hal_default_dpi = mn_hal_default_dpi * 0.6;
+	}
+
+	static lv_font_t font;
+	ret = lv_freetype_font_init(&font, hal_asset_path("fonts/RobotoCondensed-Regular.ttf"), POINTS_SCALE(32));
+	if (ret == FT_Err_Ok) {
+		mn_hal_default_font = (void *)&font;
+	}
+}
 
 int main()
 {
 #if USE_MONITOR
-#	if 1
+#	if 0
+#	elif 1
+	monitor_width = 360;
+	monitor_height = 640;
+#	elif 1
 	monitor_width = 720;
 	monitor_height = 1280;
 #   elif 0
@@ -227,14 +268,14 @@ int main()
 #endif
 
 	hal_init("./");
-	mn_hal_default_dpi = 180;
+	font_init();
 	lv_nanosvg_init();
 	lv_anim_core_init();
 
-	lv_theme_t * th = lv_theme_night_init(225, NULL);
+	lv_theme_t * th = lv_theme_tb_init(NULL, NULL);
 	lv_theme_set_current(th);
 
-	tbgui_main_window_init();
+	current_window = tbgui_main_window_init();
 
 	while (true) {
 		lv_task_handler();
